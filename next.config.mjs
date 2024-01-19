8fd054cb766dd17a07ca0e4cd99a0f8342e1c041
Georgiy <georgmarts@gmail.com>
2024-01-19 19:20:07 +0300

    2

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    formats: ["image/webp"],
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
   },
 };
 
